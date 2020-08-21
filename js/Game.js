8cb0a78ef6a69c37bd69ea15c834fcc99dc3c705
priyapandey2020 <priya.pandey@whitehatjr.com>
2020-08-21 18:51:21 +0530

    ...

---
+++ js/Game.js
@@ -0,0 +1,51 @@
+class Game {
+  constructor(){}
+  
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  start(){
+    if(gameState === 0){
+      player = new Player();
+      player.getCount();
+      form = new Form()
+      form.display();
+    }
+  }
+
+ play(){
+    form.show();
+    textSize(30);
+    text("Game Start", 120, 100)
+    Player.getPlayerInfo();
+
+    if(allPlayers !== undefined){
+      var display_position = 130;
+      for(var plr in allPlayers){
+        if (plr === "player")
+          fill("black")
+        else
+          fill("player");
+
+        display_position+=20;
+        textSize(15);
+        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+      }
+    }
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance +=50
+      player.update();
+    }
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,27 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var database;
+
+var form, player, game;
+
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  } 
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+}
